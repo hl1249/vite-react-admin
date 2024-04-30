549a63346cc9363be1cdc8434f6d1de439ef0d33
hr1249412130 <1249412130@qq.com>
2024-04-30 17:26:43 +0800

    布局设置

--- src/layout/mode/components/setting.tsx
+++ src/layout/mode/components/setting.tsx
@@ -1,10 +1,28 @@
 import React,{useState } from 'react';
 
-import {Drawer,Button } from 'antd'
+import {Drawer,Button,message } from 'antd'
 import {
   SettingOutlined
 } from '@ant-design/icons';
 
+import mode_1 from '@/assets/images/mode-1.png';
+import mode_2 from '@/assets/images/mode-2.png';
+import mode_3 from '@/assets/images/mode-3.png';
+
+const modeList = [
+  {
+    mode:"default",
+    img:mode_1
+  },
+  {
+    mode:'leftRight',
+    img:mode_2
+  },
+  {
+    mode:'topBottom',
+    img:mode_3
+  }
+]
 const Setting = () => {
 
   const [open, setOpen] = useState(false);
@@ -16,6 +34,13 @@ const Setting = () => {
   const onClose = () => {
     setOpen(false);
   };
+
+  const [mode,setMode] = useState('default')
+
+  const setLayout = (item:any) => {
+    setMode(item.mode)
+    message.success('切换成功')
+  }
   
   return(
      <div className="setting">
@@ -23,10 +48,18 @@ const Setting = () => {
       <SettingOutlined  style={{fontSize: '30px'}} />
       </Button>
      
-      <Drawer title="设置布局" onClose={onClose} open={open}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+      <Drawer width={300} title="设置布局" onClose={onClose} open={open}>
+        <h1 style={{fontWeight:'bold',fontSize:'16px'}}>选择布局</h1>
+        <div className='mode-box'>
+        {modeList.map((item,index)=>{
+          return(
+            <div className={item.mode === mode ? "mode-item active" : "mode-item"} key={index} 
+            onClick={()=>{setLayout(item)}}>
+              <img src={item.img} alt="" />
+            </div>
+          )
+        })}
+        </div>
       </Drawer>
      </div>
   )
